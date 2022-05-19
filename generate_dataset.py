fad5079d62aec73dabddefebcee3394783df218d
Justin Pinkney <justinpinkney@gmail.com>
2022-05-19 13:04:09 +0100

    parallel data gen

--- generate_dataset.py
+++ generate_dataset.py
@@ -1,25 +1,20 @@
 # Generate datasets
-
-from concurrent.futures import process
+from multiprocessing import Process
+import multiprocessing as mp
+import math
 from functools import partial
 from pathlib import Path
 from typing import Any
-import math
 
-# Simple
-# Run a generator model recording generated images and latents
-#   Generate latents (either W or W+ for stylegan)
-# Run a feature encoding model (e.g. clip) on the generated images, store the features
-
-# Complex
-# Encode real data - store original, result, and latent
-# Process with feature model
-# [Optionally] process labels with text model
-import typer
-import torch
 import clip
-from tqdm import tqdm
+import numpy as np
+import torch
+import typer
 from joblib import Parallel, delayed
+from PIL import Image
+from tqdm import tqdm
+from torchvision import transforms
+
 
 def load_sg(network_pkl):
     import sys
@@ -36,14 +31,64 @@ generators = {
 }
 
 @torch.no_grad()
+def run_folder_list(device_index, out_dir, generator_name, feature_extractor_name, out_image_size, batch_size, n_save_workers, samples_per_folder, folder_indexes):
+    device = f"cuda:{device_index}"
+    typer.echo(device_index)
+    typer.echo("Loading generator")
+    G = generators[generator_name]().to(device).eval()
+    typer.echo("Loading feature extractor")
+    feature_extractor, preprocess = clip.load(feature_extractor_name, device=device)
+    normalize = transforms.Normalize((0.48145466, 0.4578275, 0.40821073), (0.26862954, 0.26130258, 0.27577711))
+    
+    typer.echo("Generating samples")
+    with Parallel(n_jobs=n_save_workers, prefer="threads") as parallel:
+        for i_folder in folder_indexes:
+            folder_name = out_dir/f"{i_folder:05d}"
+            folder_name.mkdir(exist_ok=True)
+
+            z = torch.randn(samples_per_folder, 512, device=device)
+            w = G.mapping(z, c=None)
+            ds = torch.utils.data.TensorDataset(w)
+            loader = torch.utils.data.DataLoader(ds, batch_size=batch_size, shuffle=False, drop_last=False)
+            for batch_idx, batch in enumerate(tqdm(loader, position=device_index)):
+                out = G.synthesis(batch[0].to(device))
+
+                out = torch.nn.functional.interpolate(out, (out_image_size,out_image_size), mode="area")
+                clip_in = 0.5*torch.nn.functional.interpolate(out, (224,224), mode="area") + 0.5
+                image_features = feature_extractor.encode_image(normalize(clip_in))
+
+
+                out = out.permute(0,2,3,1).clamp(-1,1)
+                out = (255*(out*0.5 + 0.5).cpu().numpy()).astype(np.uint8)
+                image_features = image_features.cpu().numpy()
+                latents = batch[0][:,0,:].cpu().numpy()
+                parallel(
+                    delayed(process_and_save)(batch_size, folder_name, batch_idx, idx, latent, im, image_feature) 
+                    for idx, (latent, im, image_feature) in enumerate(zip(latents, out, image_features))
+                    ) 
+        typer.echo("finished folder")
+
+def process_and_save(batch_size, folder_name, batch_idx, idx, latent, im, image_feature):
+    im = Image.fromarray(im)
+    count = batch_idx*batch_size + idx
+    save(folder_name, count, latent, im, image_feature)
+
+
+def save(folder_name, idx, latent, im, image_feature):
+    basename = folder_name/f"{folder_name.stem}{idx:04}"
+    np.save(basename.with_suffix(".latent.npy"), latent)
+    im.save(basename.with_suffix(".gen.jpg"), quality=95)
+    np.save(basename.with_suffix(".img_feat.npy"), image_feature)
+
+
+
 def main(
     out_dir:Path,
     n_samples:int=1_000_000,
     generator_name:str="sg2-ffhq-1024", # TODO accept other models
     feature_extractor_name:str="ViT-B/32", # TODO accept other models
-    n_gpus:int=1,
+    n_gpus:int=2,
     out_image_size:int=256, # TODO what about non-square?
-    feat_image_size:int=224,
     feat_im_preprocess:str="resize", # TODO others e.g. nxrandcrop and average
     batch_size:int=32, # 64 struggles?
     n_save_workers:int=16,
@@ -54,62 +99,23 @@ def main(
 
     samples_per_folder = 10_000
     n_folders = math.ceil(n_samples/samples_per_folder)
-    device = "cuda:0"
+    folder_indexes = range(n_folders)
 
-    typer.echo("Loading generator")
-    G = generators[generator_name]().to(device).eval()
-    typer.echo("Loading feature extractor")
-    feature_extractor, preprocess = clip.load(feature_extractor_name, device=device)
+    sub_indexes = np.array_split(folder_indexes, n_gpus)
 
-    typer.echo("Generating samples")
-    with Parallel(n_jobs=n_save_workers) as parallel:
-        for i_folder in range(n_folders):
-            folder_name = out_dir/f"{i_folder:05d}"
-            folder_name.mkdir(exist_ok=True)
+    ps = []
+    for dev_idx, folder_list in enumerate(sub_indexes):
+        p = Process(
+            target=run_folder_list, 
+            args=(dev_idx, out_dir, generator_name, feature_extractor_name, out_image_size, batch_size, n_save_workers, samples_per_folder, folder_list),
+            )
+        p.start()
+        ps.append(p)
 
-            z = torch.randn(samples_per_folder, 512, device=device)
-            w = G.mapping(z, c=None)
-            ds = torch.utils.data.TensorDataset(w)
-            loader = torch.utils.data.DataLoader(ds, batch_size=batch_size, shuffle=False, drop_last=False)
-            for batch_idx, batch in enumerate(tqdm(loader)):
-                out = G.synthesis(batch[0].to(device))
-                clip_in = torch.nn.functional.interpolate(out, (224,224), mode="area")
-                image_features = feature_extractor.encode_image(clip_in)
-
-
-                out = torch.nn.functional.interpolate(out, (out_image_size,out_image_size), mode="area").cpu()
-                out = out.permute(0,2,3,1).clamp(-1,1)
-                out = (255*(out*0.5 + 0.5).numpy()).astype(np.uint8)
-                image_features = image_features.cpu().numpy()
-                latents = batch[0][:,0,:].cpu().numpy()
-                parallel(
-                    delayed(process_and_save)(batch_size, folder_name, batch_idx, idx, latent, im, image_feature) 
-                    for idx, (latent, im, image_feature) in enumerate(zip(latents, out, image_features))
-                    ) 
-        typer.echo("finished folder")
+    for p in ps:
+        p.join()
     typer.echo("finished all")
 
-def process_and_save(batch_size, folder_name, batch_idx, idx, latent, im, image_feature):
-    im = Image.fromarray(im)
-    count = batch_idx*batch_size + idx
-    save(folder_name, count, latent, im, image_feature)
-
-import numpy as np
-def save(folder_name, idx, latent, im, image_feature):
-    basename = folder_name/f"{folder_name.stem}{idx:04}"
-    np.save(basename.with_suffix(".latent.npy"), latent)
-    im.save(basename.with_suffix(".gen.jpg"), quality=95)
-    np.save(basename.with_suffix(".img_feat.npy"), image_feature)
-
-
-
-from PIL import Image
-def tensor2pil(t):
-    # Expect CHW
-    
-    im = Image.fromarray(im.to(torch.uint8).numpy())
-    return im
-
-
 if __name__ == "__main__":
+    mp.set_start_method('spawn')
     typer.run(main)
