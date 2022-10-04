064d4dac9642267cc715341f58a35d11161d56aa
Justin Pinkney <justinpinkney@gmail.com>
2022-10-04 06:12:54 -0400

    add missing file, start move to remote urls

--- scripts/demo.py
+++ scripts/demo.py
@@ -1,21 +1,29 @@
 import gradio as gr
 import torch
 from clip2latent import models
-from omegaconf import  OmegaConf
 
 from clip2latent import train_utils
 
 model_name = "landscape"
-use_ema = True
 device = "cuda:0"
-model_locations = {
-    "faces": ("best.ckpt", "best.yaml"),
-    "landscape": ("lhq-410-best.ckpt", "lhq-410-best.yaml"),
+model_choices = {
+    "faces": {
+        "checkpoint": "best.ckpt",
+        "config": "best.yaml",
+        "output_size": 1024,
+        },
+    "landscape": {
+        "checkpoint": "checkpoints/lhq-410-best.ckpt",
+        "config": "checkpoints/lhq-410-best.yaml",
+        "output_size": 256,
+    }
 }
-checkpoint, cfg_file = model_locations[model_name]
+checkpoint = model_choices[model_name]["checkpoint"]
+cfg_file = model_choices[model_name]["config"]
+output_size = model_choices[model_name]["output_size"]
 model = models.Clip2StyleGAN(cfg_file, device, checkpoint)
 
-n_samples = 16
+n_samples = 9
 skips = {
     "Slow": 1,
     "Med": 10,
@@ -42,7 +50,7 @@ cond_input = gr.Slider(-10, 10, 1, step=1, label="Condition scale")
 skip_timesteps = gr.Radio(list(skips.keys()), value="Fast", label="Sampling")
 select_best = gr.Radio(["Best", "All"], value="Best", label="Show best (at higher resolution) or show all.")
 
-output_image = gr.Image(shape=(1024,1024), label="Generated faces")
+output_image = gr.Image(shape=(output_size,output_size), label="Generated images")
 
 demo = gr.Interface(fn=run_model, inputs=[text_input, cond_input, skip_timesteps, select_best], outputs=output_image)
 
