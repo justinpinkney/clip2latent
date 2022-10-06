c793315e542e7bcaf1335fc2d045afe8ef8b8779
Justin <605492+justinpinkney@users.noreply.github.com>
2022-10-06 16:37:52 +0100

    move sg3 (#5)

--- clip2latent/models.py
+++ clip2latent/models.py
@@ -14,7 +14,7 @@ from clip2latent.latent_prior import LatentPrior, WPlusPriorNetwork
 
 def load_sg(network_pkl):
     import sys
-    code_folder = Path(__file__).parent.parent
+    code_folder = Path(__file__).parent
     sg3_path = str(code_folder/"stylegan3")
     sys.path.append(sg3_path)
     import dnnlib
